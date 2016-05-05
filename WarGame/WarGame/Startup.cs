9face5427c39ced91f08e4e798578ef807484b56
Leonardo Brito <leobri93@gmail.com>
2016-05-05 09:41:03 -0300

    First Commit

---
+++ WarGame/WarGame/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WarGame.Startup))]
+namespace WarGame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
