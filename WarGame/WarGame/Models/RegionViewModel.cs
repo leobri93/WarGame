63a8da5f38cfa610748f4a5774631ffd65ed6f20
Renan8 <rh.azevedo3@gmail.com>
2016-06-19 10:12:53 -0300

    Completa construção dos modelos de Kingdom, Player e Region

--- WarGame/WarGame/Models/RegionViewModel.cs
+++ WarGame/WarGame/Models/RegionViewModel.cs
@@ -1,19 +1,97 @@
-﻿namespace WarGame.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace WarGame.Models
 {
     public class RegionViewModel
     {
 
-        public int Id { get; set; }
+        private string id;
 
-        public string Name { get; set; }
+        private string name;
 
-        public int Troops { get; set; }
+        private PlayerViewModel player;
 
-        public string Src { get; set; }
+        private int troops;
 
-        public int X { get; set; }
+        private List<RegionViewModel> frontiers;
 
-        public int Y { get; set; }
+        private KingdomViewModel kingdom;
+
+        private string src;
+
+        private int x;
+
+        private int y;
+
+        public RegionViewModel(string name, string src, KingdomViewModel kingdom, int x, int y)
+        {
+            id = Guid.NewGuid().ToString("N");
+            this.name = name;
+            this.src = src;
+            this.kingdom = kingdom;
+            this.x = x;
+            this.y = y;
+            player = new PlayerViewModel("Computador", "Stark");
+            troops = 1;
+        }
+
+        public string Id
+        {
+            get { return id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public PlayerViewModel Player
+        {
+            get { return player; }
+            set { player = value; }
+        }
+
+        public int Troops
+        {
+            get { return troops; }
+            set { troops = value; }
+        }
+
+        public KingdomViewModel Kingdom
+        {
+            get { return kingdom; }
+        }
+
+        public string Src
+        {
+            get { return src; }
+        }
+
+        public int X
+        {
+            get { return x; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+        }
+
+        public void Frontiers(List<RegionViewModel> frontiers) 
+        {
+            this.frontiers = frontiers;
+        }
+
+        public RegionViewModel CheckFrontier(RegionViewModel region)
+        {
+            var index = frontiers.BinarySearch(region);
+            if (index >= 0)
+            {
+                return region.frontiers[index];
+            }
+            return null;
+        }
 
     }
 }
