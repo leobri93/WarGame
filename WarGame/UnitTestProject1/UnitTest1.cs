3aee795c836064f9ea04de04cfe25f1ed1f9f34d
Leonardo Brito <leobri93@gmail.com>
2016-07-05 08:14:51 -0300

    UnitTest class

--- WarGame/UnitTestProject1/UnitTest1.cs
+++ WarGame/UnitTestProject1/UnitTest1.cs
@@ -14,16 +14,20 @@ namespace UnitTestProject1
             // Arrange
             //var BattleTest = new Mock<WarGame.Helper.Objective>();
             //Moq.Mock<RegionViewModel> mockRegion = new Moq.Mock<RegionViewModel>();
-            var mockRegion = new Mock<RegionViewModel>();
-            var fakeRegion = new Mock<RegionViewModel>();
+            //var mockRegion = new Mock<RegionViewModel>();
+            //var fakeRegion = new Mock<RegionViewModel>();
+            var mockRegion = new Mock<KingdomViewModel>();
+            var mockPosition = new Mock<RegionPosition>();
+            var testRegion = new RegionViewModel("Teste", mockRegion.Object, mockPosition.Object);
             int[] fakeA = { 1 };
             int[] fakeD = { 6, 6, 6 };
-            var actual = mockRegion.Setup<int[]>(m=>m.Attack(fakeRegion.Object,fakeA,fakeD));
+            //var actual = mockRegion.Setup(m=>m.Attack(fakeRegion.Object,fakeA,fakeD));
+            
             //Act
             int[] expected = {0,1};
             //var MockRegion = new RegionViewModel("ataque",It.IsNotNull,It.IsNotNull);
             //Assert
-            Assert.AreEqual(expected,actual);
+            //Assert.AreEqual(expected,actual);
 
 
         }
