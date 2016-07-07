875f26d27e6e4d557a21bc59138b905966194ea5
Leonardo Brito <leobri93@gmail.com>
2016-07-06 23:51:48 -0300

    Resolved #20 - Ajuste ao verificar familia em objetivo do jogador

--- WarGame/WarGame/Models/PlayerViewModel.cs
+++ WarGame/WarGame/Models/PlayerViewModel.cs
@@ -10,7 +10,7 @@ namespace WarGame.Models
         private string id;
         private string name;
         private FamilyViewModel family;
-        private ObjectiveModel objective { get; set; }
+        public ObjectiveModel objective { get; set; }
 
         public PlayerViewModel() { }
 
